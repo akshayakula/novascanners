fed62ea785c805c1fd0b27c358a870ef964f7b33
akshayakula <47546030+akshayakula@users.noreply.github.com>
2020-12-17 21:13:18 -0500

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     description: `Leading 3D-Scan Provider in Northern`,
   },
   plugins: [
-    `@talves/gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
